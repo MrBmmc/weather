7722e6154cc77fd133583897535a427d58d4570e
Brian McMullen <clanmcmullen2012@gmail.com>
2021-06-19 12:43:39 -0400

    cleaned up code

--- assets/script.js
+++ assets/script.js
@@ -1,134 +1,114 @@
-$(document).ready(function() {
+$(document).ready(function () {
     var currentDay = moment().format("dddd, MMMM Do YYYY");
     $("#currentDay").append(currentDay);
-    
+
     var cityInputEl = $("#cityInput");
     var cityListEl = $(".cityList");
     var cityHeaderEl = $(".cityHeader");
     var currentDisplay = $(".currentInfo");
     var currentList = $(".currentList");
-    var day1El = $(".day1");
-    var day2El = $(".day2");
-    var day3El = $(".day3");
-    var day4El = $(".day4");
-    var day5El = $(".day5");
-    
+    var day1 = $(".day1");
+    var day2 = $(".day2");
+    var day3 = $(".day3");
+    var day4 = $(".day4");
+    var day5 = $(".day5");
 
-    function displayCitiesList(){
+    function displayCitiesList() {
         var cityListDisplay = JSON.parse(localStorage.getItem("cities"))
-             
+
         if (cityListDisplay) {
             for (i = 0; i < cityListDisplay.length; i++) {
-               
+
                 var cityListItem = $("<li>");
-                                
+
                 cityListItem.addClass("list-group-item");
                 cityListItem.addClass("cityClick");
-        
+
                 cityListEl.append(cityListItem);
             }
         }
     }
-    
+
     displayCitiesList();
 
-    $("#searchBtn").on("click", function(event) {
+    $("#searchBtn").on("click", function (event) {
         event.preventDefault();
 
         cityHeaderEl.empty();
         currentList.empty();
         currentDisplay.empty();
 
-        day1El.empty();
-        day2El.empty();
-        day3El.empty();
-        day4El.empty();
-        day5El.empty();
+        day1.empty();
+        day2.empty();
+        day3.empty();
+        day4.empty();
+        day5.empty();
 
-    // get value of search box and set it to the variable "cityName"
+        // get value of search box and set it to the variable "cityName"
         var cityInput = $(this).siblings("#cityInput").val();
         var cityName = cityInput.toLowerCase();
 
-    // save city on local storage
-        function saveCities(citySaved){
+        function saveCities(citySaved) {
 
             var prevCities = JSON.parse(localStorage.getItem("cities")) || [];
 
-                      
             var newCities = JSON.stringify(prevCities);
             localStorage.setItem("cities", newCities);
-            
-            // create list element with the city name and append to the ul
-            
-            
+
             var newCityListItem = $("<li>");
-                newCityListItem.text(citySaved.charAt(0) + citySaved.slice(1)) 
-                // https://flaviocopes.com/how-to-uppercase-first-letter-javascript/
-                newCityListItem.addClass("list-group-item");
-                newCityListItem.addClass("cityClick");
-                cityListEl.append(newCityListItem);
-        
-    
+            newCityListItem.text(citySaved.charAt(0) + citySaved.slice(1))
+
+            newCityListItem.addClass("list-group-item");
+            newCityListItem.addClass("cityClick");
+            cityListEl.append(newCityListItem);
+
+
         }
 
         saveCities(cityName);
-
         getData(cityName);
 
-        // clear the value of the search box
         cityInputEl.val("");
     })
 
-
-    $(document).on("click", ".cityClick", function(event) {
+    $(document).on("click", ".cityClick", function (event) {
         event.preventDefault();
 
         cityHeaderEl.empty();
         currentList.empty();
         currentDisplay.empty();
 
-        day1El.empty();
-        day2El.empty();
-        day3El.empty();
-        day4El.empty();
-        day5El.empty();
-
-
-        //console.log("city clicked");
+        day1.empty();
+        day2.empty();
+        day3.empty();
+        day4.empty();
+        day5.empty();
 
         var cityClickName = $(this).text();
         console.log(this);
         console.log(cityClickName);
-
         getData(cityClickName);
 
     })
 
+    function getData(cityChoice) {
 
-    function getData (cityChoice) {
-        
-        //fetch specific lat/long data from whatever city name is passed into this function
         let apiKey = "9bd0e1b11f3879904bad098830aa5bb2";
         var currentWeatherLink = "https://api.openweathermap.org/data/2.5/forecast?q=" + cityChoice + "&units=imperial&appid=" + apiKey;
-    
 
         fetch(currentWeatherLink)
-            
-            .then(function(response) {
+
+            .then(function (response) {
                 return response.json();
             })
 
-            //Display current weather data to weather display area
-            .then(function(data) {
-
-            // to get the info for the One Call Api you need lat/long
-            // stack overflow: https://stackoverflow.com/questions/40981040/using-a-fetch-inside-another-fetch-in-javascript 
+            .then(function (data) {
                 console.log(data);
 
                 var cityLat = data.city.coord.lat;
                 var cityLon = data.city.coord.lon;
                 var oneCall = "https://api.openweathermap.org/data/2.5/onecall?lat=" + cityLat + "&lon=" + cityLon + "&units=imperial&exclude=minutely,hourly&appid=" + apiKey;
-                
+
                 //add text content to display area
                 var cityHeader = $("<h2>");
                 cityHeader.text(data.city.name);
@@ -137,116 +117,90 @@ $(document).ready(function() {
                 return fetch(oneCall);
             })
 
-            .then(function(response) {
+            .then(function (response) {
                 return response.json();
             })
 
-            .then(function(data){
-                //console.log(data);
+            .then(function (data) {
 
-                
                 var weatherDescription = $("<li>");
                 weatherDescription.text("Current Weather: " + data.current.weather[0].description);
                 weatherDescription.addClass("list-group-item");
                 currentDisplay.append(weatherDescription);
 
-               
+
                 var weatherTemp = $("<li>");
-                weatherTemp.text("Temperature: " + data.current.temp + "°F"); // "&#8457"
+                weatherTemp.text("Temperature: " + data.current.temp + "°F");
                 weatherTemp.addClass("list-group-item");
                 currentDisplay.append(weatherTemp);
 
-                
+
                 var weatherHumid = $("<li>");
                 weatherHumid.text("Humidity: " + data.current.humidity + "%");
                 weatherHumid.addClass("list-group-item");
                 currentDisplay.append(weatherHumid);
 
-                
-                var WindSpd = $("<li>");
-                WindSpd.text("Wind Speed: " + data.current.wind_speed + "mph");
-                WindSpd.addClass("list-group-item");
-                currentDisplay.append(WindSpd);
+
+                var WindSpeed = $("<li>");
+                WindSpeed.text("Wind Speed: " + data.current.wind_speed + "mph");
+                WindSpeed.addClass("list-group-item");
+                currentDisplay.append(WindSpeed);
 
                 //UV Index
-                var UvIndx = $("<li>");
-                var rawUvIndx =$("<span>")
+                var UvIndex = $("<li>");
+                var rawUvIndex = $("<span>")
 
-                rawUvIndx.text(data.current.uvi);
-                rawUvIndx.addClass("badge");
-                UvIndx.text("UV Index: ");
-                UvIndx.addClass("list-group-item");
-                
-                // make value of the uv index an integer
-                var intUvIndx = parseInt(data.value);
+                rawUvIndex.text(data.current.uvi);
+                rawUvIndex.addClass("badge");
+                UvIndex.text("UV Index: ");
+                UvIndex.addClass("list-group-item");
 
-                if (intUvIndx >= 11) {
-                    rawUvIndx.removeClass("veryHigh");
-                    rawUvIndx.removeClass("high");
-                    rawUvIndx.removeClass("med");
-                    rawUvIndx.removeClass("low");
-                    rawUvIndx.addClass("exHigh");
-                } else if (intUvIndx < 11 && intUvIndx >= 8) {
-                    rawUvIndx.removeClass("exHigh");
-                    rawUvIndx.removeClass("high");
-                    rawUvIndx.removeClass("med");
-                    rawUvIndx.removeClass("low");
-                    rawUvIndx.addClass("veryHigh");
-                } else if (intUvIndx < 8 && intUvIndx >= 6) {
-                    rawUvIndx.removeClass("exHigh");
-                    rawUvIndx.removeClass("veryHigh");
-                    rawUvIndx.removeClass("med");
-                    rawUvIndx.removeClass("low");
-                    rawUvIndx.addClass("high");
-                } else if (intUvIndx < 6 && intUvIndx >= 4) {
-                    rawUvIndx.removeClass("exHigh");
-                    rawUvIndx.removeClass("veryHigh");
-                    rawUvIndx.removeClass("high");
-                    rawUvIndx.removeClass("low");
-                    rawUvIndx.addClass("med");
+                var intUvIndex = parseInt(data.value);
+
+                if (intUvIndex >= 7) {
+                    rawUvIndex.addClass("high");
+               
+                } else if (intUvIndex < 6 && intUvIndex >= 4) {
+
+                    rawUvIndex.addClass("med");
                 } else {
-                    rawUvIndx.removeClass("exHigh");
-                    rawUvIndx.removeClass("veryHigh");
-                    rawUvIndx.removeClass("high");
-                    rawUvIndx.removeClass("med");
-                    rawUvIndx.addClass("low");
+
+                    rawUvIndex.addClass("low");
                 }
 
-                UvIndx.append(rawUvIndx);
-                currentDisplay.append(UvIndx); 
+                UvIndex.append(rawUvIndex);
+                currentDisplay.append(UvIndex);
 
-                
-                forecast(1, day1El);
-                forecast(2, day2El);
-                forecast(3, day3El);
-                forecast(4, day4El);
-                forecast(5, day5El);
+                forecast(1, day1);
+                forecast(2, day2);
+                forecast(3, day3);
+                forecast(4, day4);
+                forecast(5, day5);
 
-                function forecast(dayNum, dayEl) {
+                function forecast(dayNum, day) {
                     var index = parseInt(dayNum)
 
                     var dayDate = $("<p>");
-                    dayDate.text(moment.unix(data.daily[index].dt).format('ddd')); 
-                    dayEl.append(dayDate);
+                    dayDate.text(moment.unix(data.daily[index].dt).format('ddd'));
+                    day.append(dayDate);
 
                     var dayIcon = $("<img>");
                     var iconcode = data.daily[index].weather[0].icon;
                     dayIcon.attr("src", "http://openweathermap.org/img/w/" + iconcode + ".png");
-                    dayEl.append(dayIcon);
+                    day.append(dayIcon);
 
                     var dayTemp = $("<p>");
                     dayTemp.text("Temp: " + data.daily[index].temp.day + "°F");
-                    dayEl.append(dayTemp);
+                    day.append(dayTemp);
 
                     var dayHumid = $("<p>");
                     dayHumid.text("Humidity: " + data.daily[index].humidity + "%");
-                    dayEl.append(dayHumid);
+                    day.append(dayHumid);
                 }
-
             })
-    }   
+    }
 
-    $(".clearBtn").click(function(){
+    $(".clearBtn").click(function () {
         localStorage.clear();
         window.location.reload();
 
